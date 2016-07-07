592801aa30eee9f9292b577cd3060b0062362f68
Eugene Zilman <ezilman@gmail.com>
2016-07-07 14:46:55 +0200

    k8s service to friendly subdomain

--- Kubernetes/Utils/AWS/route53.py
+++ Kubernetes/Utils/AWS/route53.py
@@ -1,3 +1,14 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-domain")
+parser.add_argument("-elb")
+args = parser.parse_args()
+
+print args.domain
+print args.elb
+
 import boto.ec2.elb
 import boto.route53
 from boto.route53.record import ResourceRecordSets
@@ -6,11 +17,11 @@ elb = boto.ec2.elb.connect_to_region('us-west-2')
 r53 = boto.route53.connect_to_region('us-west-2')
 
 #Ingress = 'a02a3d84a37ff11e6a23106d19ba1fed-1570681198.us-west-2.elb.amazonaws.com'
-Ingress = str(sys.argv[1])) # TODO: proper argsparse
+Ingress = args.elb
 lb = elb.get_all_load_balancers(load_balancer_names=[Ingress.split('-')[0]])[0]
 
 HOSTED_ZONE = 'Z1Y5CE8LHZN61K'
-DOMAIN_NAME = 'countly.cncfdemo.io'
+DOMAIN_NAME = args.domain
 
 rrs = ResourceRecordSets(r53,HOSTED_ZONE) 
 add_change_args_upsert = {
