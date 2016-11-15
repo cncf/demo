1ae33df00fb02482e05d1e5e6b5c0e598ca55719
Eugene Zilman <ezilman@gmail.com>
2016-11-15 05:59:08 -0800

    reorg

--- cncfdemo-cli/cncfdemo/bootstrap/aws/cli.py
+++ cncfdemo-cli/cncfdemo/bootstrap/aws/cli.py
@@ -70,7 +70,7 @@ def cli():
 @click.option('--region', default='us-west-2')
 @click.option('--scale', default=1)
 @click.option('--KeyName', default='cncf-aws')
-@click.option('--ImageId', default='ami-a95af8c9')
+@click.option('--ImageId', default='ami-5c27853c')
 @click.option('--SecurityGroups', default=['cncfdemo'], multiple=True)
 @click.pass_context
 def aws(ctx, region, scale, \
