446625ef1d339f9d4cf797d95ce6df630a52b020
Eugene Zilman <ezilman@gmail.com>
2017-01-04 23:30:31 -0600

    Proper timeformatting and timezones support

--- Web/api/_src/new.py
+++ Web/api/_src/new.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 
 import os
-import time
+import time, datetime
 import json
-
-from hashids import Hashids
 
 import jsonschema
 from jsonschema import validate
+from hashids import Hashids
+
 
 def loadFiles(path='./Schemas'):
     files = [j for j in os.listdir(path) if j.endswith('.json')]
@@ -29,21 +29,41 @@ def respond(body=None, err=None):
 
 def handler(event, context):
 
-    print("Received event: " + json.dumps(event, indent=2))
+    print("Received event: " + json.dumps(event, indent=2))  # logs to CloudWatch
+
+    try:
+      body = json.loads(event.get('body'))
+    except (ValueError, TypeError):
+      err = type('err', (object,), {})
+      err = err()
+      err.message = 'Body must be valid json'
+      return respond(err=err)
+
     hashids = Hashids()
     schema = loadFiles()
 
     try:
-      validate(event, schema)
+      validate(body, schema)
     except (jsonschema.exceptions.SchemaError, jsonschema.exceptions.ValidationError) as err:
       return respond(err=err)
 
-    uuid = hashids.encode(int(time.time()))
-    return respond(body=uuid)
+    now = int(time.time())  # Collision if two demos start at exactly same second
+    human = datetime.datetime.fromtimestamp(now).strftime('%a, %d %B %Y - %H:%M UTC')
+
+    body['Metadata']['id'] = hashids.encode(now)
+    body['Metadata']['timestart'] = now
+
+    body['events'] = [{ "title": "",
+                        "raw": r"""<span class="event-message">Demo Started On {}</span>""".format(human),
+                     }]
+
+    return respond(body=body)
+
 
 if __name__ == '__main__':
 
   context = {}
   event = loadFiles('../_tests/new')
+  event['body'] = json.dumps(event['body'])
 
   print(handler(event, context))
