1c732af749976782e7d97ce7424f9cc3c786cd3e
Eugene Zilman <ezilman@gmail.com>
2017-01-05 06:17:18 -0600

    Proper timeformatting and timezones support

--- Web/api/_src/new.py
+++ Web/api/_src/new.py
@@ -8,8 +8,10 @@ import jsonschema
 from jsonschema import validate
 from hashids import Hashids
 
+import boto3
 
-def loadFiles(path='./Schemas'):
+
+def load_files(path='./Schemas'):
     files = [j for j in os.listdir(path) if j.endswith('.json')]
     for f in files:
       with open(os.path.join(path, f)) as json_file:
@@ -20,11 +22,17 @@ def loadFiles(path='./Schemas'):
 def respond(body=None, err=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(body),
+        'body': json.dumps(err.message) if err else json.dumps(body),
         'headers': {
             'Content-Type': 'application/json',
         },
     }
+
+
+def store_data(Key, Body, Metadata = {'foo': 'bar'}, ContentType='application/json'):
+  s3 = boto3.resource('s3')
+  bucket = s3.Bucket('stats.cncfdemo.io')
+  bucket.put_object(ACL='public-read', Key=Key, Body=Body, ContentType=ContentType, Metadata=Metadata)
 
 
 def handler(event, context):
@@ -39,8 +47,8 @@ def handler(event, context):
       err.message = 'Body must be valid json'
       return respond(err=err)
 
-    hashids = Hashids()
-    schema = loadFiles()
+    hashids = Hashids(salt='grabfromenv')
+    schema = load_files()
 
     try:
       validate(body, schema)
@@ -57,13 +65,14 @@ def handler(event, context):
                         "raw": r"""<span class="event-message">Demo Started On {}</span>""".format(human),
                      }]
 
+    store_data(body['Metadata']['id'], json.dumps(body))
     return respond(body=body)
 
 
 if __name__ == '__main__':
 
   context = {}
-  event = loadFiles('../_tests/new')
+  event = load_files('../_tests/new')
   event['body'] = json.dumps(event['body'])
 
   print(handler(event, context))
