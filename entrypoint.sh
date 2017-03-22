8fff3ba62835389fea2d24796d3075d30b6c0e4c
DLX <denver@ii.org.nz>
2017-03-23 01:41:00 +1300

    Create new Deploy Based on Name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@
 set -e
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-export TF_VAR_name="$2"
 
 # Run CMD
+export TF_VAR_name="$2"
 if [ "$1" = "azure" ] ; then
     terraform get /azure && \
     terraform apply -target null_resource.sshkey_gen /azure && \
@@ -15,7 +15,7 @@ if [ "$1" = "azure" ] ; then
     terraform apply -target null_resource.cloud_gen /azure && \
     terraform apply -target module.dns.null_resource.dns_gen /azure && \
     terraform apply -target module.etcd.azurerm_network_interface.cncf /azure && \
-    time terraform apply /azure && printf "${RED}\n#Commands to Configue Kubectl \n\n" && printf 'sudo chown -R $(whoami):$(whoami) $(pwd)/data/ \n\n' && printf 'export KUBECONFIG=$(pwd)/data/kubeconfig \n\n'${NC}
+    time terraform apply /azure && printf "${RED}\n#Commands to Configue Kubectl \n\n" && printf 'sudo chown -R $(whoami):$(whoami) $(pwd)/data/${name} \n\n' && printf 'export KUBECONFIG=$(pwd)/data/${name}/kubeconfig \n\n'${NC}
 elif [ "$1" = "azure-destroy" ] ; then
     time terraform destroy -force /azure
 fi
