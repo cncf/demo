697ee579d8fb6cee62311a5ee212cc5908799051
DLX <denver@ii.org.nz>
2017-03-23 01:50:52 +1300

    Update Deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 
 # Run CMD
 export TF_VAR_name="$2"
-if [ "$1" = "azure" ] ; then
+if [ "$1" = "deploy" ] ; then
     terraform get /azure && \
     terraform apply -target null_resource.sshkey_gen /azure && \
     terraform apply -target null_resource.ssl_gen /azure && \
@@ -16,7 +16,7 @@ if [ "$1" = "azure" ] ; then
     terraform apply -target module.dns.null_resource.dns_gen /azure && \
     terraform apply -target module.etcd.azurerm_network_interface.cncf /azure && \
     time terraform apply /azure && printf "${RED}\n#Commands to Configue Kubectl \n\n" && printf 'sudo chown -R $(whoami):$(whoami) $(pwd)/data/${name} \n\n' && printf 'export KUBECONFIG=$(pwd)/data/${name}/kubeconfig \n\n'${NC}
-elif [ "$1" = "azure-destroy" ] ; then
+elif [ "$1" = "destroy" ] ; then
     time terraform destroy -force /azure
 fi
 
