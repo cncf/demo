c0975ee5d69bb3a4606313723c9ef26622a119f1
DLX <denver@ii.org.nz>
2017-04-10 05:19:10 +1200

    Add etcd Host and Create 3 Nodes based on Count

--- modules/etcd/input.tf
+++ modules/etcd/input.tf
@@ -1,26 +1,29 @@
-variable "location" {}
-variable "subnet-id" {}
+# variable "location" {}
+# variable "subnet-id" {}
 variable "name" {}
-variable "master-vm-size" {}
+variable "region" {}
+variable "zone" {}
+variable "project" {}
+# variable "master-vm-size" {}
 variable "master-node-count" {}
-variable "image-publisher" {}
-variable "image-offer"     {}
-variable "image-sku"       {}
-variable "image-version"   {}
-variable "availability-id" {}
-variable "storage-account" {}
-variable "storage-primary-endpoint" {}
-variable "storage-container" {}
-variable "k8s-apiserver-tar" {}
-variable "cluster-domain" {}
-variable "dns-service-ip" {}
-variable "internal-tld" {}
-variable "pod-cidr" {}
-variable "service-cidr" {}
-variable "admin-username" {}
-variable "kubelet-image-url" {}
-variable "kubelet-image-tag" {}
-variable "cloud-config" {}
+# variable "image-publisher" {}
+# variable "image-offer"     {}
+# variable "image-sku"       {}
+# variable "image-version"   {}
+# variable "availability-id" {}
+# variable "storage-account" {}
+# variable "storage-primary-endpoint" {}
+# variable "storage-container" {}
+# variable "k8s-apiserver-tar" {}
+# variable "cluster-domain" {}
+# variable "dns-service-ip" {}
+# variable "internal-tld" {}
+# variable "pod-cidr" {}
+# variable "service-cidr" {}
+# variable "admin-username" {}
+# variable "kubelet-image-url" {}
+# variable "kubelet-image-tag" {}
+# variable "cloud-config" {}
 
 # variable "etcd-security-group-id" {}
 # variable "external-elb-security-group-id" {}
--- modules/etcd/nodes.tf
+++ modules/etcd/nodes.tf
@@ -1,53 +1,90 @@
-resource "azurerm_network_interface" "cncf" {
-  count = "${ var.master-node-count }"
-  name                = "etcd-interface${ count.index + 1 }"
-  location            = "${ var.location }"
-  resource_group_name = "${ var.name }"
+resource "google_compute_instance" "cncf" {
+  count        = "${ var.master-node-count }"
+  name         = "${ var.name }${ count.index + 1 }"
+  machine_type = "n1-standard-1"
+  zone         = "${ var.zone }"
 
-  ip_configuration {
-    name                          = "etcd-nic${ count.index + 1 }"
-    subnet_id                     = "${ var.subnet-id }"
-    private_ip_address_allocation = "dynamic"
-    # private_ip_address            = "${ element( split(",", var.etcd-ips), count.index ) }"
-    load_balancer_backend_address_pools_ids = ["${ azurerm_lb_backend_address_pool.cncf.id }"]
+  tags = ["foo", "bar"]
+
+  disk {
+    image = "debian-cloud/debian-8"
+  }
+
+  // Local SSD disk
+  disk {
+    type    = "local-ssd"
+    scratch = true
+  }
+
+  network_interface {
+    # network = "${ var.name }"
+    subnetwork = "${ var.name }"
+    subnetwork_project = "${ var.project }"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
 
-resource "azurerm_virtual_machine" "cncf" {
-  count = "${ var.master-node-count }"
-  name                  = "etcd-master${ count.index + 1 }"
-  location              = "${ var.location }"
-  availability_set_id   = "${ var.availability-id }"
-  resource_group_name = "${ var.name }"
-  network_interface_ids = ["${ element(azurerm_network_interface.cncf.*.id, count.index) }"] 
-  vm_size               = "${ var.master-vm-size }"
+# resource "azurerm_network_interface" "cncf" {
+#   count = "${ var.master-node-count }"
+#   name                = "etcd-interface${ count.index + 1 }"
+#   location            = "${ var.location }"
+#   resource_group_name = "${ var.name }"
 
-  storage_image_reference {
-    publisher = "${ var.image-publisher }"
-    offer     = "${ var.image-offer }"
-    sku       = "${ var.image-sku }"
-    version   = "${ var.image-version}"
-  }
+#   ip_configuration {
+#     name                          = "etcd-nic${ count.index + 1 }"
+#     subnet_id                     = "${ var.subnet-id }"
+#     private_ip_address_allocation = "dynamic"
+#     # private_ip_address            = "${ element( split(",", var.etcd-ips), count.index ) }"
+#     load_balancer_backend_address_pools_ids = ["${ azurerm_lb_backend_address_pool.cncf.id }"]
+#   }
+# }
 
-  storage_os_disk {
-    name          = "etcd-disks${ count.index + 1 }"
-    vhd_uri       = "${ var.storage-primary-endpoint }${ var.storage-container }/etcd-vhd${ count.index + 1 }.vhd"
-    caching       = "ReadWrite"
-    create_option = "FromImage"
-  }
+# resource "azurerm_virtual_machine" "cncf" {
+#   count = "${ var.master-node-count }"
+#   name                  = "etcd-master${ count.index + 1 }"
+#   location              = "${ var.location }"
+#   availability_set_id   = "${ var.availability-id }"
+#   resource_group_name = "${ var.name }"
+#   network_interface_ids = ["${ element(azurerm_network_interface.cncf.*.id, count.index) }"] 
+#   vm_size               = "${ var.master-vm-size }"
 
-  os_profile {
-    computer_name  = "etcd-master${ count.index + 1 }"
-    admin_username = "${ var.admin-username }"
-    admin_password = "Password1234!"
-    custom_data = "${ element(data.template_file.cloud-config.*.rendered, count.index) }"
-  }
+#   storage_image_reference {
+#     publisher = "${ var.image-publisher }"
+#     offer     = "${ var.image-offer }"
+#     sku       = "${ var.image-sku }"
+#     version   = "${ var.image-version}"
+#   }
 
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path = "/home/${ var.admin-username }/.ssh/authorized_keys"
-      key_data = "${file("/cncf/data/.ssh/id_rsa.pub")}"
-  }
- }
-}
+#   storage_os_disk {
+#     name          = "etcd-disks${ count.index + 1 }"
+#     vhd_uri       = "${ var.storage-primary-endpoint }${ var.storage-container }/etcd-vhd${ count.index + 1 }.vhd"
+#     caching       = "ReadWrite"
+#     create_option = "FromImage"
+#   }
+
+#   os_profile {
+#     computer_name  = "etcd-master${ count.index + 1 }"
+#     admin_username = "${ var.admin-username }"
+#     admin_password = "Password1234!"
+#     custom_data = "${ element(data.template_file.cloud-config.*.rendered, count.index) }"
+#   }
+
+#   os_profile_linux_config {
+#     disable_password_authentication = true
+#     ssh_keys {
+#       path = "/home/${ var.admin-username }/.ssh/authorized_keys"
+#       key_data = "${file("/cncf/data/.ssh/id_rsa.pub")}"
+#   }
+#  }
+# }
