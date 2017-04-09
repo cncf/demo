eba41d9e909788e3d93935d347769f8c8210d50d
DLX <denver@ii.org.nz>
2017-04-10 08:06:24 +1200

    Create Private DNS Zone for ETCD Cluster

--- modules/dns/input.tf
+++ modules/dns/input.tf
@@ -1,6 +1,5 @@
 # variable "depends-id" {}
 variable "internal-tld" {}
 variable "name" {}
-variable "name-servers-file" { default = "azure_dns_zone"}
-variable "master-ips" { type = "list" }
+# variable "master-ips" { type = "list" }
 # variable "vpc-id" {}
