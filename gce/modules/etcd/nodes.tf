bb0b060d14a56a9c6bd5639ff7d158953a369573
DLX <denver@ii.org.nz>
2017-04-11 12:22:12 +1200

    Revert Cloud-init Changes

--- gce/modules/etcd/nodes.tf
+++ gce/modules/etcd/nodes.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance" "cncf" {
   }
 
   metadata {
-    user-data = "${ element(data.template_cloudinit_config.myconfig.*.rendered, count.index) }"
+    user-data = "${ element(data.template_file.cloud-config.*.rendered, count.index) }"
   }
 
   service_account {
