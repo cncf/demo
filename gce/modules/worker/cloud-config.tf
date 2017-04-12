0fb8ff0bc830ce41abd15acf8e13dbe1af48136b
DLX <denver@ii.org.nz>
2017-04-13 06:05:24 +1200

    Write Master Certs to Disk

--- gce/modules/worker/cloud-config.tf
+++ gce/modules/worker/cloud-config.tf
@@ -7,8 +7,9 @@ data "template_file" "cloud-config" {
     kubelet-image-url = "${ var.kubelet-image-url }"
     kubelet-image-tag = "${ var.kubelet-image-tag }"
     internal-tld = "${ var.internal-tld }"
-    location = "${ var.location }"
-    k8s-worker-tar = "${ base64encode(var.k8s-worker-tar) }"
+    ca = "${ var.ca }"
+    k8s-worker = "${ var.k8s-worker }"
+    k8s-worker-key = "${ var.k8s-worker-key }"
     cloud-config = "${ base64encode(var.cloud-config) }"
   }
 }
