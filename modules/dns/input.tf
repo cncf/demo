8723bbbe7ca7636c4a4f8a5e4c8255029918585a
DLX <denver@ii.org.nz>
2017-04-16 12:10:50 +1200

    Override resolv.conf

--- modules/dns/input.tf
+++ modules/dns/input.tf
@@ -3,6 +3,7 @@ variable "internal-tld" {}
 variable "name" {}
 variable "master-ips" { type = "list" }
 variable "master-node-count" { default = "3" }
+variable "name-servers-file" {}
 
 
 # variable "vpc-id" {}
