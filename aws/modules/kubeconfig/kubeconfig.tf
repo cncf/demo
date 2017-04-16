73142be4d93068a122c0ff538faa8d940d934e9f
Chris McClimans <chris@hippiehacker.org>
2017-04-17 09:12:21 +1200

    Removing old kubeconfig approach

--- aws/modules/kubeconfig/kubeconfig.tf
+++ aws/modules/kubeconfig/kubeconfig.tf
@@ -1,26 +1,3 @@
-data "template_file" "kubeconfig" {
-  template = <<EOF
-kubectl config set-cluster cluster-${ var.name } \
-  --embed-certs=true \
-  --server=https://${ var.master_elb } \
-  --certificate-authority=${ var.ca_pem }
-
-kubectl config set-credentials admin-${ var.name } \
-  --embed-certs=true \
-  --certificate-authority=${ var.ca_pem } \
-  --client-key=${ var.admin_key_pem } \
-  --client-certificate=${ var.admin_pem }
-
-kubectl config set-context ${ var.name } \
-  --cluster=cluster-${ var.name } \
-  --user=admin-${ var.name }
-kubectl config use-context ${ var.name }
-
-# Run this command to configure your kubeconfig:
-# eval $(terraform output kubeconfig)
-EOF
-}
-
 resource "null_resource" "kubeconfig" {
 
   provisioner "local-exec" {
