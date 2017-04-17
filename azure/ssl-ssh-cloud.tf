0f708d7982a68c9720c5c2c39dbc006ef87a02fe
DLX <denver@ii.org.nz>
2017-04-17 18:59:34 +1200

    Add Prereqs to a Single Resource so -target is only needed once

--- azure/ssl-ssh-cloud.tf
+++ azure/ssl-ssh-cloud.tf
@@ -1,5 +1,5 @@
 #Gen Certs and SSH KeyPair
-resource "null_resource" "ssl_ssh_gen" {
+resource "null_resource" "ssl_ssh_cloud_gen" {
 
   provisioner "local-exec" {
     command = <<EOF
@@ -33,9 +33,38 @@ EOF
     rm -rf ${ var.data_dir }/.ssh
 EOF
   }
+
+  provisioner "local-exec" {
+    command = <<COMMAND
+cat <<JSON > ${ var.data_dir }/azure-config.json
+{
+  "aadClientId": "$${ARM_CLIENT_ID}",
+  "aadClientSecret": "$${ARM_CLIENT_SECRET}",
+  "tenantId": "$${ARM_TENANT_ID}",
+  "subscriptionId": "$${ARM_SUBSCRIPTION_ID}",
+  "resourceGroup": "${ var.name }",
+  "location": "${ var.location }",
+  "subnetName": "${ var.name }",
+  "securityGroupName": "${ var.name }",
+  "vnetName": "${ var.name }",
+  "routeTableName": "${ var.name }",
+  "primaryAvailabilitySetName": "${ var.name }"
+}
+JSON
+COMMAND
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+    on_failure = "continue"
+    command = <<EOF
+    rm -rf ${ var.data_dir }/azure-config.json
+    EOF
+  }
+
 }
 
 resource "null_resource" "dummy_dependency" {
-  depends_on = [ "null_resource.ssl_ssh_gen" ]
+  depends_on = [ "null_resource.ssl_ssh_cloud_gen" ]
 }
 
