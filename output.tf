2a069db283fef0f28747316c8b33cc75f8eef9b3
DLX <denver@ii.org.nz>
2017-04-20 16:57:43 +1200

    Create ENV File for KubectlA

--- output.tf
+++ output.tf
@@ -12,7 +12,7 @@
 #output "cluster-domain" { value = "${ var.cluster-domain }" }
 #output "dns-service-ip" { value = "${ var.dns-service-ip }" }
 #output "etcd1-ip" { value = "${ element( split(",", var.etcd-ips), 0 ) }" }
-#output "external-elb" { value = "${ module.etcd.external-elb }" }
+output "external-elb" { value = "${ module.etcd.external-lb }" }
 #output "internal-tld" { value = "${ var.internal-tld }" }
 #output "name" { value = "${ var.name }" }
 #output "region" { value = "${ var.aws["region"] }" }
