b403f0536ea5998429ecb0a39e31f65d0ba0ee20
DLX <denver@ii.org.nz>
2017-04-20 12:06:11 +1200

    Remove Cloud-provider=azure

--- packet/modules/worker/output.tf
+++ packet/modules/worker/output.tf
@@ -1,5 +1,2 @@
 #output "fqdn_lb" { value = "${azurerm_public_ip.cncf.fqdn}" }
-output "first_worker_ip" { value = "${ packet_device.workers.0.network.0.address }" }
-output "second_worker_ip" { value = "${ packet_device.workers.1.network.0.address }" }
-output "third_worker_ip" { value = "${ packet_device.workers.2.network.0.address }" }
-output "worker_ips" { value = ["${ packet_device.workers.*.network.0.address }"] }
+output "public_worker_ips" { value = ["${ packet_device.workers.*.network.0.address }"] }
