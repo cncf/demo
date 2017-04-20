5ccd1171c2250f13c7ee0ed025084440ec9ab194
Chris McClimans <chris@hippiehacker.org>
2017-04-21 07:15:01 +1200

    Use the same kubeconfig module for all clouds

--- aws/modules/kubeconfig/io.tf
+++
@@ -1,7 +0,0 @@
-variable "admin_key_pem" {}
-variable "admin_pem" {}
-variable "ca_pem" {}
-variable "master_elb" {}
-variable "name" {}
-variable "data_dir" {}
-
--- cross-cloud/cloud.tf
+++ cross-cloud/cloud.tf
@@ -18,3 +18,33 @@ module "packet" {
   data_dir                  = "${ var.data_dir }/packet"
   packet_project_id         = "${ var.packet_project_id }"
 }
+
+module "aws-kubeconfig" {
+  source = "../kubeconfig"
+  admin_key_pem = "${ var.data_dir }/aws/.cfssl/k8s-admin-key.pem"
+  admin_pem = "${ var.data_dir }/aws/.cfssl/k8s-admin.pem"
+  ca_pem = "${ var.data_dir }/aws/.cfssl/ca.pem"
+  data_dir = "${ var.data_dir }"
+  fqdn_k8s = "${ module.aws.external_elb }"
+  name = "${ var.name }"
+}
+
+module "azure-kubeconfig" {
+  source = "../kubeconfig"
+  admin_key_pem = "${ var.data_dir }/azure/.cfssl/k8s-admin-key.pem"
+  admin_pem = "${ var.data_dir }/azure/.cfssl/k8s-admin.pem"
+  ca_pem = "${ var.data_dir }/azure/.cfssl/ca.pem"
+  data_dir = "${ var.data_dir }"
+  fqdn_k8s = "${ module.azure.fqdn_k8s }"
+  name = "${ var.name }"
+}
+
+module "packet-kubeconfig" {
+  source = "../kubeconfig"
+  admin_key_pem = "${ var.data_dir }/packet/.cfssl/k8s-admin-key.pem"
+  admin_pem = "${ var.data_dir }/packet/.cfssl/k8s-admin.pem"
+  ca_pem = "${ var.data_dir }/packet/.cfssl/ca.pem"
+  data_dir = "${ var.data_dir }"
+  fqdn_k8s = "${ module.azure.fqdn_k8s }"
+  name = "${ var.name }"
+}
