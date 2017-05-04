cb152650eb64e45e2014a4d0b3f871537beda747
Denver <denver@debian.nz>
2017-05-04 19:39:57 +0000

    Sleep for 10 on Destroy

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -63,6 +63,7 @@ elif [ "$1" = "gke-destroy" ] ; then
     terraform get ${DIR}/gke && \
     terraform destroy -force -target module.cluster.google_container_node_pool.cncf ${DIR}/gke && \
     terraform destroy -force -target module.cluster.google_container.cncf ${DIR}/gke && \
+    sleep 10 && \
     time terraform destroy -force ${DIR}/gke
     
 elif [ "$1" = "cross-cloud-deploy" ] ; then
