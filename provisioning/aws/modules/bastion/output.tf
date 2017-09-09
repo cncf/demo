f1cae4951aecf0100c6cd506cfad9ea55fd467f7
Eugene Zilman <ezilman@gmail.com>
2017-09-09 14:22:44 +0900

    icecreamd for firefox

--- provisioning/aws/modules/bastion/output.tf
+++
@@ -1,2 +0,0 @@
-output "depends_id" { value = "${null_resource.dummy_dependency.id}" }
-output "ip" { value = "${ aws_instance.bastion.public_ip }" }
--- provisioning/gce/modules/etcd/output.tf
+++
@@ -1,4 +0,0 @@
-output "external_lb" { value = "${ google_compute_forwarding_rule.external.ip_address }" }
-
-output "internal_lb" { value = "${ google_compute_forwarding_rule.cncf.ip_address }" }
-
